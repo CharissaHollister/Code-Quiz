7a3a2fe456155fd38d95d1b77a0a77f72dde1ddc
CharissaHollister <charissa.hollister@diasorin.com>
2022-05-09 23:08:40 -0500

    add more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 ////---------Global variables-----------////
 
-//--------Section variables-----------
+//--------Section variables-----------//
 
 var highScoreLink = document.querySelector(".leftHeader");
 var TimerArea = document.querySelector(".rightHeader");
@@ -13,10 +13,9 @@ var startButtonSection = document.querySelector(".startButtonsArea");
 var answerAreaSection = document.querySelector(".answerArea");
 var endAreaSection = document.querySelector(".endArea");
 var highScoresPageSection = document.querySelector(".highScoresPage");
-
 //never hidden: titleIntro, Header, footer
 
-//------------item variables---------
+//------------item variables---------//
 
 var timerEl = document.getElementById("countDown");
 var titleBlockVar = document.getElementById("titleBlock");
@@ -25,17 +24,14 @@ var answer1Button = document.querySelector("#answer1");
 var answer2Button = document.querySelector("#answer2");
 var answer3Button = document.querySelector("#answer3");
 var answer4Button = document.querySelector("#answer4");
-//var rightWrongSection = document.getElementById('rightWrong');
 var submitButton = document.getElementById("submitBtn");
 var submitScoreButton = document.getElementById("submitScoreBtn");
 var initialsSection = document.getElementById("initials");
-// var scoreTitleSection = document.getElementById('scoreTitle');
-// var yourScoreArea = document.getElementById('scoreYours');
 var wrongAnswerArea = document.getElementById("wrongAnswer");
 var goBackButton = document.querySelector("#goBack");
 var clearScoresButton = document.querySelector("#clearScores");
 
-//-------value variables-------
+//--------------value variables-----------//
 
 var userScore = {
   // name: "",
@@ -48,7 +44,6 @@ if (userScorePrev) {
 } else {
   userScoreSets = [];
 }
-
 var i = 0;
 var answerSet = {};
 var rightWrong = 0;
@@ -58,7 +53,7 @@ var userInitials = "";
 var highScoreResult = "";
 var timeInterval;
 
-//-----------quiz questions array------------
+//-----------quiz questions array------------//
 const myQuestions = [
   {
     question: "Who invented JavaScript?",
@@ -81,20 +76,91 @@ const myQuestions = [
     correctAnswer: "C",
   },
   {
-    question: "Which tool can you use to ensure code quality?",
+    question: "Inside which HTML element do we put the JavaScript?",
     answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
+      a: "<js>",
+      b: "<javascript>",
+      c: "<script>",
+      d: "<scripting>",
+    },
+    correctAnswer: "C",
+  },
+  {
+    question: 'What is the correct JavaScript syntax to change the content of the HTML element below? <p id="demo">This is a demonstration.</p>',
+    answers: {
+      a: "demo.innerHTML = 'Hello World!'",
+      b: "document.getElementByName('p').innerHTML = 'Hello World!'",
+      c: "document.getElement('p').innerHTML = 'Hello World!'",
+      d: "document.getElementById('demo').innerHTML = 'Hello World!'",
     },
     correctAnswer: "D",
   },
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: {
+      a: "function myFunction()",
+      b: "function = myFunction()",
+      c: "function:myFunction()",
+      d: "function(MyFunction);",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    answers: {
+      a: "if i == 5 then",
+      b: "if (i ==5){}",
+      c: "i i=5 then()",
+      d: "none of the above",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: {
+      a: "The <body> section",
+      b: "The <head> section",
+      c: "Both the <head> section and the <body> section are correct",
+      d: "Neither of these",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question: "How can you add a comment in a JavaScript??",
+    answers: {
+      a: "<!--This is a comment-->",
+      b: "'This is a comment'",
+      c: "//This is a comment",
+      d: "append.note: This is a comment",
+    },
+    correctAnswer: "C",
+  },
+  {
+    question: "Will a function automatically run?",
+    answers: {
+      a: "Yes; They will run in order from top of the script file",
+      b: "No; They must be called later in the script file",
+      c: "Yes; They will run as long as they are capitalized",
+      d: "No; They must be called in HTML",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "Which of these is accurate of FOR LOOP statements?",
+    answers: {
+      a: "for (loop time, array length, action to var)",
+      b: "for (starting point, loop until, action to var)",
+      c: "for (action to var, starting point, loop time)",
+      d: "for (array start, array end, action to array",
+    },
+    correctAnswer: "B",
+  },
 ];
 
-//---------define functions---------
 
-//--------timer-----------
+////---------define functions---------////
+
+//--------timer, starts at 180 seconds-----------//
 function countdown() {
   timeInterval = setInterval(function () {
     // the timer still has more than 1 second and states "# of seconds remaining"
@@ -114,7 +180,7 @@ function countdown() {
   }, 1000);
 }
 
-//-----------when to show which sections---------
+////-----------when to show which sections---------////
 function hideShowStart() {
   wrongAnswerArea.textContent = " ";
   highScoreLink.style.display = "block";
@@ -156,9 +222,9 @@ function hideShowHighScores() {
   highScoresPageSection.style.display = "block";
 }
 
-//----- player's results--------
+////----- player's results--------////
 
-//------If you run out of time page--------
+//------If you run out of time page--------//
 function failedPage() {
   highScoreLink.style.display = "none";
   TimerArea.style.display = "block";
@@ -171,16 +237,32 @@ function failedPage() {
   wrongAnswerArea.textContent = "Refresh and Try Again";
 }
 
+//------- end page to enter initials and see score-------//
 function finalPage() {
+  hideShowEnd();
   //grabs time at quiz completion
   clearInterval(timeInterval);
   titleBlockVar.textContent = "All Done!";
+  //current players score
   var scoreTitleSection = document.getElementById("scoreTitle");
   scoreTitleSection.textContent = "Your Score is " + timeLeft;
 }
 
-//----- test is player picked the correct answer for each question
+//-------what question to pull from array---------//
 
+function displayQuestion() {
+  if (i < 3) {
+    titleBlockVar.textContent = myQuestions[i].question;
+    answer1Button.textContent = myQuestions[i].answers.a;
+    answer2Button.textContent = myQuestions[i].answers.b;
+    answer3Button.textContent = myQuestions[i].answers.c;
+    answer4Button.textContent = myQuestions[i].answers.d;
+  } else {
+    finalPage();
+  }
+}
+
+//----- test if player picked the correct answer for each question---//
 function answerCreation() {
   var answerOptions = document.querySelectorAll('input[name="answerSelect"]');
   console.log(answerOptions);
@@ -199,6 +281,7 @@ function answerCreation() {
   } else {
     answerNow = null;
   }
+  //error if no answer
   if (answerNow == null) {
     wrongAnswerArea.textContent = "must select one answer";
   } else if (myQuestions[i].correctAnswer === answerNow) {
@@ -212,33 +295,19 @@ function answerCreation() {
     i++;
   } else {
     rightWrong = rightWrong + 1;
-    ////add to minus 10 seconds from timer //
+    ////point to background rightWrong var
+    //add to minus 10 seconds from timer
     timeLeft = timeLeft - 10;
     wrongAnswerArea.textContent = "Incorrect";
   }
   displayQuestion();
 }
 
-//-------what question to pull from array---------
-
-function displayQuestion() {
-  if (i < 3) {
-    titleBlockVar.textContent = myQuestions[i].question;
-    answer1Button.textContent = myQuestions[i].answers.a;
-    answer2Button.textContent = myQuestions[i].answers.b;
-    answer3Button.textContent = myQuestions[i].answers.c;
-    answer4Button.textContent = myQuestions[i].answers.d;
-  } else {
-    hideShowEnd();
-    finalPage();
-  }
-}
-
 //--------------highscores page------------
 function highScoresPage() {
   //grab initials from final page
   userInitials = document.getElementById("initials").value;
-  //userInitials = userInitials.toUpperCase
+ 
   if (userInitials == "") {
     alert("You need to fill out your initials!");
     finalPage();
@@ -249,11 +318,11 @@ function highScoresPage() {
     var yourScoreArea = document.getElementById("scoreYours");
     yourScoreArea.textContent = "Your High Score is " + userScore.highScore;
   }
+  //merge and sort all scores array
   userScoreSets.push(userScore);
   userScoreSets.sort(function (a, b) {
     return b.highScore - a.highScore;
   });
-
   localStorage.setItem("userScoreSets", JSON.stringify(userScoreSets));
 
   hideShowHighScores();
@@ -262,14 +331,8 @@ function highScoresPage() {
     "Congrats " + userScore.name + " your score is: " + userScore.highScore;
 
   ////create div elements to list all the high scores from overallscores array
-  ////no idea if this is correct, fix later
   var listScores = document.querySelector(".highScoresList");
-
-  // var li1 = document.createElement("li");
-  // var c = 0
-
   var c = 0;
-
   while (c < userScoreSets.length) {
     var li = document.createElement("li");
     li.classList.add("pastScores");
@@ -285,17 +348,18 @@ function highScoresPage() {
   }
 }
 
-//// Go Back button -- returns to start page
+// Go Back button -- returns to start page
 function goBack() {
   location.reload();
 }
 
-//// clear all scores button -- clear local storage
+// clear all scores button -- clear local storage
 function clearScores() {
   localStorage.clear();
   location.reload();
 }
 
+// high scores page from header link
 function highScoresPageNoResults() {
   highScoresPage();
   yourScoreArea.style.display = "none";
@@ -311,118 +375,3 @@ submitScoreButton.addEventListener("click", highScoresPage);
 goBackButton.addEventListener("click", goBack);
 clearScoresButton.addEventListener("click", clearScores);
 highScoreLink.addEventListener("click", highScoresPageNoResults);
-
-//---------------------old code attempts---------------------
-
-// var goBack = document.querySelector('#goBack');
-// goBack.addEventListener("click", openURL);
-// check localStorage for high score, if it's not there, use 0
-
-// var yourScoreSection = document.getElementById('endScore')
-// // if (highScore === null) {
-// var highScore = 0;
-// yourScoreSection.textContent = "highscore";
-//}
-
-//var yourScore = localStorage.setItem("yourScore");
-
-// function finalscore(){
-//     //calculate final score
-
-// }
-
-// var answerA = document.querySelectorAll("input[type='radio'][name='answer1Select']:checked");
-// console.log(answerA)
-// console.log(answerB)
-// console.log(answerC)
-// console.log(answerD)
-// var answerRecord = JSON.stringify(answerNow);
-// localStorage.setItem("lastAnswer", answerRecord);
-// answers.push(answerRecord)
-
-//return(answerSet)}
-// i++;
-// displayQuestion();
-
-// if(answerA == true){
-//     var answerNow = ([i] + "A");
-// }
-// var answerRecord = JSON.stringify(answerNow);
-// localStorage.setItem("lastAnswer", answerRecord);
-// answers.push(answerRecord)
-// // console.log(userScore)
-// console.log(answers)
-// return answers;
-// }
-//let selectedSize;
-// for (const radioButton of answerOptions) {
-// if (answerSelect[1].checked) {
-//     var answerNow = ([i] + "A");
-//     console.log(answerNow);
-// }
-//     }
-// answerSet.a = answer1Button
-// answers.push(answerSet)
-// const btn = document.querySelector('#btn');
-//     const radioButtons = document.querySelectorAll('input[name="size"]');
-//     btn.addEventListener("click", () => {
-//         let selectedSize;
-//         for (const radioButton of radioButtons) {
-//             if (radioButton.checked) {
-//                 selectedSize = radioButton.value;
-//                 break;
-//             }
-//         }
-// answer1Select.click(
-// answer1Select = true)
-// console.log(answer1Select);
-
-// if(answer1Select == true){
-// var answerNow = ([i] + "A");
-// console.log(answerNow);
-// // answerSet[].appendChild(answerNow);
-// }
-//console.log(answerSet)
-//if b
-
-//if c
-
-//id d
-// for(var i=0; i<questions.length; i++){
-
-//     // first reset the list of answers
-//     answers = [];
-
-//     // for each available answer...
-//     for(letter in myQuestions[i].answers){
-
-//       // ...add an html radio button
-//       answers.push(
-//         '<label>'
-//           + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-//           + letter + ': '
-//           + myQuestions[i].answers[letter]
-//         + '</label>'
-//       );
-//     }
-
-//     displayQuestion();
-// }
-
-// userScoreSets.appendChild(userScore);
-// var taskNameInput = document.querySelector("input[name='task-name']").value;
-// check if input values are empty strings
-//   if (!taskNameInput || !taskTypeInput) {
-//     alert("You need to fill out your initials!");
-//     return false;
-//    // yourScoreSection.textContent = (highScore);
-// function enterName(){
-//     document.querySelector("input[name='initialsEntry']").value = initials;
-// }
-//     introSection.textContent = 'High Scores';
-//document.querySelector("input[name='initialsEntry']");
-
-//     userInitials.setAttribute("data-task-id", taskId);
-//     var yourScoreSection = document.getElementById('endScore');
-// var highScore = 0;
-// yourScoreSection.textContent = (highScore);
